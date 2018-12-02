0e9f7a3d2e4b4173348faaceecd5eb22ec991a5a
Naoki Nakajima <n2.jima@gmail.com>
2018-12-02 11:54:30 +0900

    remove unused files

--- bot_response.js
+++
@@ -1,7 +0,0 @@
-function BotResponse(spreadsheetRow) {
-    this.triggerWord = spreadsheetRow[0];
-    this.text = spreadsheetRow[1];
-    this.userName = spreadsheetRow[2];
-    this.iconUrl = spreadsheetRow[3];
-    this.iconEmoji = spreadsheetRow[4];
-};
